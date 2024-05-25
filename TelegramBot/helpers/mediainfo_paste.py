5594c35829bf28455a020b33969f8d6efd63d88e
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-05-25 12:06:42 +0530

    Update mediainfo_paste.py

--- TelegramBot/helpers/mediainfo_paste.py
+++ TelegramBot/helpers/mediainfo_paste.py
@@ -256,6 +256,7 @@ def html_builder(title: str, text: str) -> str:
 
 def mediainfo_paste(text: str, title: str) -> str:
     html_content = html_builder(title, text)
+    print("meida info on go")
     URL = "https://mediainfo-1-y5870653.deta.app/api"
     response = requests.post(URL, json={"content": html_content})
     if response.status_code == 200:
